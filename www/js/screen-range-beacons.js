90c672d71c3564475bd683869b5efb9111f5283c
Jimmi Vila <jimmivco@hotmail.com>
2016-12-01 14:19:36 -0600

    v1.1

--- www/js/screen-range-beacons.js
+++ www/js/screen-range-beacons.js
@@ -77,40 +77,14 @@
 	};
 
 	function callMedia(beacon){
-		// console.log($('#beacon-media').prop('ended'));
-		// console.log($('#beacon-media').prop('duration'));
-		// console.log($('#beacon-media').prop('src'));
 
 		if(!findByMacAddress(beacon.macAddress)){
 
-			// get beacon media reproducir
-			console.log('tome su audio');
 
-			//***** $('#beacon-media').prop('src','images/audio/CakeByTheOcean.mp3')
 			//test
 			$.get( "http://koko-test.com/click/back-end/index.php/app/test", function( data ) {
 				$('#beacon-media').append('<p>'+data+'</p>');
 			});			
-
-			// localStorage.setItem('nowPlaying',beacon.macAddress);
-
-			// console.log(JSON.stringify($('#beacon-media')));
-
-			// aumentar el contador en el api aqui
-
-			// $.ajax
-		 //    ({
-		 //        type: "POST",
-		 //        //the url where you want to sent the userName and password to
-		 //        url: 'http://koko-test.com/click/back-end/index.php/app/saveEvent',
-		 //        dataType: 'application/json',
-		 //        async: false,
-		 //        //json object to sent to the authentication url
-		 //        data: JSON.stringify({id_channel:"null",channel_name:"null",event_name:"null",id_rule:"null",rule_name:"null",id_object:"null"}),
-		 //        success: function () {
-		 //        	$('#beacon-media').append('<p>went better than expected</p>');
-		 //        }
-		 //    });
 
 		 	$.post( "http://koko-test.com/click/back-end/index.php/app/saveEvent", {id_channel:"null",channel_name:"null",event_name:"null",id_rule:"null",rule_name:"null",id_object:beacon.macAddress})
 			  .done(function( data ) {
@@ -126,10 +100,6 @@
 	}
 
 	function findByMacAddress(macAddress){
-		//console.log(macAddress);
-		// console.log(beacons);
-		// beacons.push('adios');
-		// localStorage.setItem('beacons',JSON.stringify(beacons));
 
 		beaconInList = false;
 
